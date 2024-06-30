cc19fc1c7efbae5501076108dc8ebb02cff5ff24
nekorionebula <ardiansyahrafael5@gmail.com>
2024-06-30 12:51:00 +0700

    feat: CLI, sleep duration

--- kill.go
+++ kill.go
@@ -9,7 +9,11 @@ import (
 	"time"
 )
 
-func getProcessID(processName string) (int, error) {
+func getSleepDuration(duration float64) interface{} {
+	return time.Duration(duration) * time.Second
+}
+
+func getProcessID(processName string, duration float64) (int, error) {
 	//Run "tasklist" command
 	cmd := exec.Command("tasklist")
 	output, err := cmd.Output()
@@ -27,22 +31,26 @@ func getProcessID(processName string) (int, error) {
 			//Check if the first field (process name) is an exact match
 			if strings.EqualFold(fields[0], processName) {
 				//convert the second field (PID) to an integer
-				pid, err := strconv.Atoi(fields[1])
-				if err != nil {
-					return 0, fmt.Errorf("failed to retriece %v id", err.Error())
+				pid, pidErr := strconv.Atoi(fields[1])
+				if pidErr != nil {
+					return 0, fmt.Errorf("failed to retriece %v id", pidErr.Error())
 				}
+				//Sleep Duration before return
+				fmt.Printf("%v will be terminated after %.2f minutes\n", processName, duration/60)
+				sleepDuration := getSleepDuration(duration)
+				time.Sleep(sleepDuration.(time.Duration))
 				return pid, nil
 			}
 		}
 	}
 	if scanner.Err() != nil {
-		return 0, fmt.Errorf("failed to scan %v", err.Error())
+		return 0, fmt.Errorf("failed to scan %v", err)
 	}
 	return 0, fmt.Errorf("%v is not found", processName)
 }
 
-func kill(processName string) {
-	pid, err := getProcessID(processName)
+func kill(processName string, duration float64) {
+	pid, err := getProcessID(processName, duration)
 	if err != nil {
 		fmt.Println("Error:", err.Error())
 		return
@@ -56,13 +64,27 @@ func kill(processName string) {
 }
 
 func main() {
-	processName := "spotify.exe"
+	var (
+		processName string
+		duration    float64
+	)
+	fmt.Print("App name: ")
+	_, err := fmt.Scanf("%s\n", &processName)
+	if err != nil {
+		fmt.Println("Error :", err.Error())
+		return
+	}
+	fmt.Print("The app will be terminated in (s): ")
+	_, err = fmt.Scanf("%f\n", &duration)
+	if err != nil {
+		fmt.Println("Error :", err.Error())
+		return
+	}
 	//pid, err := getProcessID(processName)
 	//if err != nil {
 	//	fmt.Println("Error:", err)
 	//} else {
 	//	fmt.Println("Process ID: ", pid)
 	//}
-	time.Sleep(5 * time.Second)
-	kill(processName)
+	kill(processName, duration)
 }
