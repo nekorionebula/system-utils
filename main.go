badefd543ad8e6dc45d250dd9cba80f78429852e
nekorionebula <ardiansyahrafael5@gmail.com>
2024-07-02 20:52:34 +0700

    feat: add shutdown s time in task

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/nekorionebula/system-utils/internal/kill"
 )
 
+func main() {
+	Input()
+}
+
 var (
 	input string
 	try   string
@@ -12,7 +16,8 @@ var (
 
 func Input() {
 	fmt.Println("List of Utilities:")
-	fmt.Println("1. Kill App")
+	fmt.Printf("1. Kill App\n2. Shutdown\n")
+	fmt.Print("Task number: ")
 	_, err := fmt.Scanf("%s\n", &input)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -20,20 +25,18 @@ func Input() {
 	switch input {
 	case "1":
 		kill.RunKill()
+	case "2":
+		kill.Shutdown()
 	default:
 		fmt.Println("Input correct number.")
 		fmt.Println("Try again? (y/n): ")
 		_, _ = fmt.Scanf("%s\n", &try)
-		switch try {
-		case "y", "Y", "yes", "Yes", "YES":
-			fmt.Println()
-			Input()
-		case "n", "N", "no", "No", "NO":
+
+		if try != "y" {
+			fmt.Println("Exiting...")
 			return
+		} else {
+			Input()
 		}
 	}
-}
-
-func main() {
-	Input()
 }
